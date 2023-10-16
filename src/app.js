9acc9ab9bfe654932b29cd41b55be69f7209a621
helenaraimundo91 <139161478+helenaraimundo91@users.noreply.github.com>
2023-10-16 19:08:51 +0000

    execuse generator completed

--- src/app.js
+++ src/app.js
@@ -5,7 +5,33 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector("#excuse").innerHTML = generateExcuse();
+};
+
+let generateExcuse = () => {
+  let who = ["Voldemort", "Godzilla", "The queen", "Gollum"];
+  let actions = ["stole", "destroyed", "burned", "run away with"];
+  let possetion = ["my phone", "my wallet", "my bag", "my keys"];
+  let where = [
+    "at the supermarket",
+    "on the beach",
+    "on the street",
+    "at my home"
+  ];
+
+  let whoIndex = Math.floor(Math.random() * who.length);
+  let actionsIndex = Math.floor(Math.random() * actions.length);
+  let possetionIndex = Math.floor(Math.random() * possetion.length);
+  let whereIndex = Math.floor(Math.random() * where.length);
+
+  return (
+    who[whoIndex] +
+    " " +
+    actions[actionsIndex] +
+    " " +
+    possetion[possetionIndex] +
+    " " +
+    where[whereIndex]
+  );
 };
